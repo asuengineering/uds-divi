bdc8f212256574646e760cfa0a4bb05fb82a8964
Steve Ryan <steve.ryan@asu.edu>
2021-02-25 14:41:14 -0700

    Getting SASS to compile correctly. Creating minified styles for theme.
    Enqueue the

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@ function uds_divi_enqueue_scripts() {
 	$child_css_version = $theme_version . '.' . filemtime( get_stylesheet_directory() . '/style.css' );
 
 	wp_register_style( 'divi-style', get_template_directory_uri() . '/style.css', array(), $css_version );
-	wp_register_style( 'uds-divi-style', get_stylesheet_uri(), array( 'divi-style' ), $child_css_version );
+	wp_register_style( 'uds-divi-style', get_stylesheet_directory_uri() . '/css/uds-divi.css', array( 'divi-style' ), $child_css_version );
 
 	wp_enqueue_style( 'divi-style' );
 	wp_enqueue_style( 'uds-divi-style' );
